4b752e945974fc14f877c40c451438131291ee0e
Marcus Davanco <md.daves@gmail.com>
2022-11-12 18:19:21 -0300

    feat: Componente: Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Pagination } from "../../components/Form/Pagination";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
@@ -81,6 +82,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
