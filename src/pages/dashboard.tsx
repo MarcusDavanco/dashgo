c3ab0f86157cd1d1a2762b2c3bd22828cb36edec
Marcus Davanco <md.daves@gmail.com>
2022-11-13 20:59:34 -0300

    fix: ApexChart display

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 const Chart = dynamic(() => import("react-apexcharts"), {
@@ -56,32 +57,50 @@ const options = {
 
 const series = [{ name: "series1", data: [31, 120, 10, 28, 61, 18, 109] }];
 export default function Dashboard() {
+  const [assembleGraphics, setAssembleGraphics] = useState(false);
+
+  useEffect(() => {
+    setAssembleGraphics(true);
+  });
+
   return (
     <Flex direction="column" h="100vh">
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
-        <SimpleGrid
-          flex="1"
-          gap="4"
-          minChildWidth="320px"
-          alignItems="flex=start"
-        >
-          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Inscritos da semana
-            </Text>
-            {/* @ts-ignore-next-line */}
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Taxa de abertura
-            </Text>
-            {/* @ts-ignore-next-line */}
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-        </SimpleGrid>
+        {assembleGraphics && (
+          <SimpleGrid
+            flex="1"
+            gap="4"
+            minChildWidth="320px"
+            alignItems="flex=start"
+          >
+            <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Inscritos da semana
+              </Text>
+              {/* @ts-ignore-next-line */}
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
+            <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Taxa de abertura
+              </Text>
+              {/* @ts-ignore-next-line */}
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
+          </SimpleGrid>
+        )}
       </Flex>
     </Flex>
   );
